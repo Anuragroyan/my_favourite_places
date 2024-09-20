010062f1bfdfce859d971360d97b939138baa920
Anurag Roy <38952781+Anuragroyan@users.noreply.github.com>
2024-09-21 02:30:29 +0530

    api key is updated

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyDeFgUwB74q4iTiw7R0y2mSebUu7-EwxYE";
+const GOOGLE_API_KEY = "AIzaSyD_5b70h2I2vAnDL0qBKB9RCKqlYa0iLwk";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
